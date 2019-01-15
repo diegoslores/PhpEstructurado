7a93b271fda387df6eb17fff09f3ebb6dbc361fb
dawMP - Sánchez Lores <A17DiegoSL1@sanclemente.local>
2019-01-15 21:50:11 +0100

    Corrección de errores menores.

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
-//TODO: Realiza aquí la 
-
 <html>
 <head>
   <title>Alumnos y Profesores</title>
 </head>
 <body>
-  <?php echo "<br/>hola<br/>"; 
+  <?php 
   $alumnos = [
     ['nombre'=> 'Eugenio', 'apellido' => 'Martínez', 'notas' => []],
     ['nombre'=> 'Marta', 'apellido' => 'Carrera', 'notas' => []],
     ['nombre'=> 'Nacho', 'apellido' => 'Herrera', 'notas' => []],
     ['nombre'=> 'Anxo', 'apellido' => 'Iglesias', 'notas' => []],
     ['nombre'=> 'Valentina', 'apellido' => 'Iglesias', 'notas' => []]
-  ];   
+  ];
+  echo "
+  <hr>
+  <pre>Nombre                       Apellido</pre>
+  <hr>
+  ";   
   echo $alumnos; 
   ?>
 </body>  
