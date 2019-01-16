811b0b564772ef0e6016c62eed706a4a1477f984
dawMP - Sánchez Lores <A17DiegoSL1@sanclemente.local>
2019-01-16 19:46:47 +0100

    funcion imprimirAlumnos iniciada.

--- index.php
+++ index.php
@@ -11,15 +11,25 @@
     ['nombre'=> 'Anxo', 'apellido' => 'Iglesias', 'notas' => []],
     ['nombre'=> 'Valentina', 'apellido' => 'Iglesias', 'notas' => []]
   ];
-  echo "
-  <hr>
-  <pre>Nombre       Apellido</pre>
-  <hr><ol>
-  ";   
-  foreach($alumnos as $alumno){
-    echo "<li><pre>".$alumno['nombre']."</pre></li>";
-  }; 
-  echo "</ol>"
+  
+  imprimirAlumnos($alumnos);
+   function imprimirAlumnos($datos){
+    echo "
+    <hr>
+    <pre>Nombre       Apellido</pre>
+    <hr><ol>
+    ";       
+    foreach($datos as $alumno){
+      echo "<li><pre>".$alumno['nombre']."</pre></li>";
+    }; 
+    echo "</ol>";
+    echo "<ol>";  
+     
+    foreach($datos as $alumno){
+      echo "<li><pre>".$alumno['apellido']."</pre></li>";
+    }; 
+    echo "</ol>";
+  }
   ?>
 </body>  
 </html>
