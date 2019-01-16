1908c68f02d17d74a09ffd3f0847c8906e889843
dawMP - Sánchez Lores <A17DiegoSL1@sanclemente.local>
2019-01-16 20:20:15 +0100

    Mejora de codigo.

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <html>
 <head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Alumnos y Profesores</title>
+  <link rel="stylesheet" type="text/css" href="css.css" />
 </head>
 <body>
   <?php 
@@ -8,27 +11,23 @@
     ['nombre'=> 'Eugenio', 'apellido' => 'Martínez', 'notas' => []],
     ['nombre'=> 'Marta', 'apellido' => 'Carrera', 'notas' => []],
     ['nombre'=> 'Nacho', 'apellido' => 'Herrera', 'notas' => []],
-    ['nombre'=> 'Anxo', 'apellido' => 'Iglesias', 'notas' => []],
-    ['nombre'=> 'Valentina', 'apellido' => 'Iglesias', 'notas' => []]
+    ['nombre'=> 'Anxo', 'apellido' => 'Iglesias', 'notas' => []]
   ];
   
   imprimirAlumnos($alumnos);
    function imprimirAlumnos($datos){
     echo "
     <hr>
-    <pre>Nombre       Apellido</pre>
-    <hr><ol>
+    <div class='nomApe'><pre>Nombre\t\tApellido</pre></div>
+    <hr>
+    <div class='lista'>
+    <ol>
     ";       
     foreach($datos as $alumno){
-      echo "<li><pre>".$alumno['nombre']."</pre></li>";
+      echo "<li><pre>".$alumno['nombre']. "\t\t" .$alumno['apellido']."</pre></li>";
     }; 
-    echo "</ol>";
-    echo "<ol>";  
-     
-    foreach($datos as $alumno){
-      echo "<li><pre>".$alumno['apellido']."</pre></li>";
-    }; 
-    echo "</ol>";
+    echo "</ol></div><hr>";
+    
   }
   ?>
 </body>  
