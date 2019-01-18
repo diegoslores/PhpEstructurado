486ee0e30a1c34a62085028fd8ad0659daef7e7b
dawMP - Sánchez Lores <A17DiegoSL1@sanclemente.local>
2019-01-18 21:41:49 +0100

    Correcciones menores.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     ['nombre'=> 'Nacho', 'apellido' => 'Herrera', 'notas' => []],
     ['nombre'=> 'Anxo', 'apellido' => 'Iglesias', 'notas' => []]
   ];
-  
+  //La primera imprimir una lista de sus alumnos (el programa ya tiene una lista precargada de alumnos).
   imprimirAlumnos($alumnos);
    function imprimirAlumnos($datos){
     echo "
@@ -26,9 +26,17 @@
     foreach($datos as $alumno){
       echo "<li><pre>".$alumno['nombre']. "\t\t" .$alumno['apellido']."</pre></li>";
     }; 
-    echo "</ol></div><hr>";
-    
+    echo "</ol></div><hr>";    
   }
   ?>
 </body>  
 </html>
+<?php/*
+La segunda, añade un nuevo alumno pidiendo los datos necesarios (nombre y apellidos inicialmente).
+La tercera simula un examen de los alumnos existentes (la simulación de las notas se realiza para evitar introducir estas notas manualmente para cada uno, recuerda que es un problema didáctico que no tiene por qué ajustarse a la realidad). En la shell se puede ver un submenú donde se parametriza la dificultad del examen para que las notas se ajusten a este.
+Si la dificultad es baja: Se generará la nota (para cada alumno y con 2 decimales) con un nº aleatorio entre 5 y 10
+Si la dificultad es media: Se generará la nota (para cada alumno y con 2 decimales) con un nº aleatorio entre 0 y 10
+Si la dificultad es alta: Se generará la nota (para cada alumno y con 2 decimales) gerando un primer nº aleatorio entre 0 y 10, luego un segudo nº aleatorio también entre 0 y 10 y finalmente, la nota será el nº mínimo de ambos.
+La cuarta opción representa la lista de los alumnos junto con sus notas.
+La quinta opción cierra el aplicativo.
+*/?>
