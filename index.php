6089991c8c8bf6c8e4b5899d5ea00218c402b0cd
Diegos Lores <diegoslores@gmail.com>
2019-01-24 09:50:14 +0100

    Session isset en array alumnos

--- index.php
+++ index.php
@@ -9,15 +9,20 @@
 <body>
   <header class="cabecera">
     <?php
-
+    if(!isset($_SESSION['alumnos'])){
+		$_SESSION['alumnos'] = [
+   ['nombre'=> 'Eugenio', 'apellido' => 'Martínez', 'notas' => []],
+   ['nombre'=> 'Marta', 'apellido' => 'Carrera', 'notas' => []],
+   ['nombre'=> 'Nacho', 'apellido' => 'Herrera', 'notas' => []],
+   ['nombre'=> 'Anxo', 'apellido' => 'Iglesias', 'notas' => []]
+ ];
     if(isset($_GET['salir'])){
 			if($_GET['salir']=="Salir"){
 				session_destroy();
       	echo '<script>location.href="index.php"</script>';
-        }
+      }
 		}
-    if(session_status() == PHP_SESSION_ACTIVE and existeUsuarioAsociado()){
-
+    if(session_status() == PHP_SESSION_ACTIVE){
           imprimeBotonAlumnos();
           imprimeBotonMatricula();
           imprimeBotonExamen();
@@ -49,9 +54,6 @@
 </body>
 </html>
 <?php
-function existeUsuarioAsociado(){
-	 		return $_SESSION['nombre']=='user' and $_SESSION['contraseña']=='123';
-	 	}
 
     //sesion array de Alumnos
     //sesion user
@@ -109,12 +111,5 @@ function imprimirAlumnos($datos){
     echo "<li><pre>".$alumno['nombre']. "\t\t" .$alumno['apellido']."</pre></li>";
   };
   echo "</ol></div></div>";
-}/*
-La segunda, añade un nuevo alumno pidiendo los datos necesarios (nombre y apellidos inicialmente).
-La tercera simula un examen de los alumnos existentes (la simulación de las notas se realiza para evitar introducir estas notas manualmente para cada uno, recuerda que es un problema didáctico que no tiene por qué ajustarse a la realidad). En la shell se puede ver un submenú donde se parametriza la dificultad del examen para que las notas se ajusten a este.
-Si la dificultad es baja: Se generará la nota (para cada alumno y con 2 decimales) con un nº aleatorio entre 5 y 10
-Si la dificultad es media: Se generará la nota (para cada alumno y con 2 decimales) con un nº aleatorio entre 0 y 10
-Si la dificultad es alta: Se generará la nota (para cada alumno y con 2 decimales) gerando un primer nº aleatorio entre 0 y 10, luego un segudo nº aleatorio también entre 0 y 10 y finalmente, la nota será el nº mínimo de ambos.
-La cuarta opción representa la lista de los alumnos junto con sus notas.
-La quinta opción cierra el aplicativo.
-*/?>
+}
+?>
