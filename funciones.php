ec2a3eb022c030a80e97ad99f736912f14e5621a
Diegos Lores <diegoslores@gmail.com>
2019-02-10 12:19:31 +0100

    Resultado final del ejercicio.

--- funciones.php
+++ funciones.php
@@ -4,6 +4,7 @@
  * las funciones que se llaman al pulsar los botones excepto la de cerrar sesion.
  */
 
+ //Función que mediante un for each recorre el array de alumnos y los imprime en una tabla.
  function imprimirAlumnos($datos){
   echo "<table id='tableAlumnos'>
     <tbody><tr> <th>Nombre</th><th>Apellido</th>";  
@@ -13,7 +14,7 @@
     echo "</tbody></table>";
   }
    
-
+//Formulario pat¡ra matricular alumnos.
  function formMatricularAlumno(){
    echo "<h3><pre>2.Matricular Alumnos</pre></h3>
    <form class='formMatricula' action='index.php' method='POST'>
@@ -23,6 +24,7 @@
  	 </form>";
  }
 
+ //Array que coge los datos de matrícula y los mete en el array de alumnos.
  function procesarDatosMatricula($array){
    if(isset($_POST['addAlumn'])){
       if(!empty($_POST['name']) and !empty($_POST['surname'])){
@@ -36,6 +38,7 @@
     return $array;
  }
 
+ //Formulario para seleccionar el nivel de dificultas del examen que generará notas.
  function simuladorExamen(){
    echo "<h3><pre>3.Simulador de Examen</pre></h3>
    <form class='formExam' action='index.php' method='POST'>
@@ -53,11 +56,14 @@
    </label>
    </form>";
  }
+
+ // Función para generar notas de forma aleatoria. se le pasa dos valores (nota mínima y nota máxina) y un tercero como múltiplo para redondeos.
  function rand_float($st_num,$end_num,$mul){
   if ($st_num>$end_num) return false;
     return mt_rand($st_num*$mul,$end_num*$mul)/$mul;
   }
 
+ //Para el examen difícil hay que generar dos notas y compararlas para elegir la menor. Eso se hace con esta función.
   function getMinValue($float1, $float2){
     if ($float1 < $float2) { 
       return $float1;  
@@ -66,6 +72,7 @@
     }
   }
 
+ //función de generación de notas en función del nivel de dificultad elegido. Se aplica las dos funciones anteriores para que salga un valor u otro.
  function addScore($array){
   if(isset($_POST['generateExam'])){
     foreach($array as $k => &$item){
@@ -81,13 +88,17 @@
   return $array;
  }
 
+ //Función para imprimir todos los datos del alumno, incluidas notas y medias en una tabla.
  function printAlumnosNotas($datos){
+  //Se imprime la cabecera de la tabla.
   echo "<table id='tablaexamenes'>
     <tbody><tr> <th>Nombre</th><th>Apellido</th>";
+    //Con este for se imprime el número de nota que se va a representar debajo, nota1, nota2 ....
     for ($i = 1; $i <= count($datos[0]["notas"]); $i++) {
       echo "<th> Nota".$i."</th>";
     }
     echo "<th> Nota Media </th></tr>";
+    //En este bucle se empieza a recorrer el array de alumnos y a imprimir cada dato, nombre, apellido, cada nota que tenga y la media final. Cada uno debajo de su cabecera.
     foreach($datos as $alumn){
       echo "<tr> <td> ".$alumn["nombre"]." </td><td> ".$alumn["apellido"]." </td> ";
       foreach($alumn["notas"] as $nota){
@@ -100,6 +111,7 @@
     }
       echo "</tbody></table>";
   }
+  //Función que calcula la nota media del array de notas del alumno.
   function notaMedia($notas){
     $sumaNotas = array_sum($notas);
     $totalNotas = count($notas);
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Alumnos y Profesores</title>
-  <link rel="stylesheet" type="text/css" href="css.css" />
+  <link rel="stylesheet" type="text/css" href="./css.css" />
 </head>
 <body>
   <header>
@@ -28,12 +28,14 @@
         ['nombre'=> 'Anxo', 'apellido' => 'Iglesias', 'notas' => []]
       ];
     }
+    //Con esta función iniciamos todo el aplicativo de gestión de alumnos. Pasando como parametro el array en el que guardamos los usuarios permitidos a login.
     init($usuarios);
     ?>
   </main>
 </body>
 </html>
 <?php
+//con esta función comprobamos que el usuario que se está logueando existe. en caso afirmativo se entra en el aplicativo. En caso cotrario seguira el formulario de login.
 function validar($value){
   if(!isset($_POST['usuario'])){
       if($_POST['username'] and $_POST['password'] and $_POST['username'] == $value[0]['username']and $_POST['password'] == $value[0]['password']){
@@ -57,7 +59,7 @@ function login(){
   <input type='submit' name='login' value='Enviar'>
   </form>";
 }
-//Botones de selección;
+//Botones de selección de funcionalidades del aplicativo;
 function imprimeBotones(){
   echo "<form action='index.php' method='POST'>
       <input type='submit' name='menu' value='Alumnos'>
@@ -67,7 +69,7 @@ function imprimeBotones(){
       <input type='submit' name='menu' value='LogOut'>
     </form>";
 }
-//función de cerrar sesiones.
+//función de cerrar sesion.
 function cerrarSesion(){
   if(isset($_POST['menu'])){
     if($_POST['menu']=='LogOut'){
@@ -76,6 +78,7 @@ function cerrarSesion(){
     }
   }
 }
+//Esta función una vez dentro del aplicativo añade a cada iteración las ttres lineas de cabezera. Es para ahorrar lineas de código.
 function headerInit(){
   imprimeBotones();
   $alumnos = $_SESSION['alumnos'];
@@ -83,10 +86,13 @@ function headerInit(){
   <main class='cuerpo'>";
   return $alumnos;
 }
-
+//Función principal del programa. En ella se ejecutan todas las peticiones del usuario al pulsar un botón. Se le pasa como parámetro la lista de usuarios permitidos.
 function init($usuarios){
+  //En el primer if responde al botón de login. Si este es pulsado entra en la función validar.
   if (isset($_POST['login'])) {
     validar($usuarios);
+  //Una vez dentro del aplicativo cuando pulsamos uno de los cinco botones del menú (isset menú) entra dentro de este if que esta compuesto con un switch en el que 
+  //cada case responde ante cada botón del menú.
   }else  if(isset($_POST["menu"])){
     $alumnos = headerInit();
     switch($_POST["menu"]){
@@ -108,12 +114,13 @@ function init($usuarios){
         cerrarSesion();
         break;
     }
+  //Una vez dentro del submenu matricular existe otro formulario que responde ante este if si pulsas el botón de añadir. Aqui se procesa el añadido de un nuevo alumno a la lista.
   }else  if(isset($_POST["addAlumn"])){
     $alumnos = headerInit();
     $_SESSION['alumnos'] = procesarDatosMatricula($alumnos);
     formMatricularAlumno();
-    imprimirAlumnos($_SESSION['alumnos']);
-        
+    imprimirAlumnos($_SESSION['alumnos']);   
+  //En el submenú de examen al pulsar el botón de generar notas entra en este if y ejecuta las funciones para generar un examen y añadir las notas a los alumnos.     
   }else  if(isset($_POST["generateExam"])){
     $alumnos = headerInit();
     $_SESSION['alumnos'] = addScore($alumnos);
@@ -123,12 +130,8 @@ function init($usuarios){
     printAlumnosNotas($_SESSION['alumnos']);
     echo "</div></div>";       
   }else {
+    //En caso de que los isset anteriores no existan se imprime el login.
       login();
   }
 }
-
-/*}else if(($_POST['usuario']!='user' and $_POST['contraseña']!='123') or $_POST['usuario']!='user' or $_POST['contraseña']!='123'){
-  echo '<div id="incompleto"><pre>Login Erroneo</pre></div>';
-  login();
-}*/
 ?>
